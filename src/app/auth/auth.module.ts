329146945c73d30f5b4e6680949c55af4529382a
masete <nicholasmasete72@gmail.com>
2022-02-12 11:44:24 +0300

    added the not found and forgotten pswd component to the auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ForgottenPasswordComponent } from './forgotten-password/forgotten-password.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    ForgottenPasswordComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule
